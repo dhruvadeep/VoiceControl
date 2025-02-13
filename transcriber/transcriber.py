a102dde0a6f6dcf39511e9140a6330af6404eb49
udithdoddapaneni <udithdoddapaneni@gmail.com>
2025-02-13 22:39:36 +0530

    minor bug fixes in commands function

--- transcriber/transcriber.py
+++ transcriber/transcriber.py
@@ -26,21 +26,21 @@ except:
 
 #incomplete: need to add more commands
 COMMANDS = [
-    (["search for", "search", "bing"], search),
-    (["open browser", "open page", "open new page"], open_browser),
-    (["close browser", "shutdown browser"], close_browser),
-    (["close current window", "close window"], close_current_window),
-    (["open camera"], open_camera),
-    (["take screenshot", "take a screenshot"], screenshot),
-    (["show ram info", "get ram info", "show ram usage", "get ram usage"], get_ram),
-    (["show disk info", "get disk info", "show disk usage", "get disk usage"], get_disk),
-    (["show cpu usage", "get cpu usage"], get_cpu_usage),
-    (["show cpu info", "get cpu info"], get_cpu_info),
-    (["show system info", "get system info", "show hardware info", "get hardware info"], get_all)
+    (["search for", "search", "bing"], "search"),
+    (["open browser", "open page", "open new page"], "open_browser"),
+    (["close browser", "shutdown browser"], "close_browser"),
+    (["close current window", "close window"], "close_current_window"),
+    (["open camera"], "open_camera"),
+    (["take screenshot", "take a screenshot"], "screenshot"),
+    (["show ram info", "get ram info", "show ram usage", "get ram usage"], "get_ram"),
+    (["show disk info", "get disk info", "show disk usage", "get disk usage"], "get_disk"),
+    (["show cpu usage", "get cpu usage"], "get_cpu_usage"),
+    (["show cpu info", "get cpu info"], "get_cpu_info"),
+    (["show system info", "get system info", "show hardware info", "get hardware info"], "get_all")
 ]
 
 @validate_call
-def commands(transcription: str) -> dict|None:
+def commands(transcription: str) -> dict[str:list[dict[str:str]]]|None:
     transcription = transcription.lower()
     user_instructions = []
     for i in transcription.split("and"):
@@ -49,10 +49,14 @@ def commands(transcription: str) -> dict|None:
                 user_instructions.append(k)
     responses = []
     for user_instruction in user_instructions:
-        for commands, fn in COMMANDS:
-            for command in commands:
-                if command in user_instruction:
-                    response = fn(command, user_instruction)
+        for queries, command in COMMANDS:
+            for query in queries:
+                if query in user_instruction:
+                    additional_information = user_instruction.split(query)[1]
+                    response = {
+                        "command": command,
+                        "additional": additional_information
+                    }
                     responses.append(response)
                     break
     return {"response":responses}
@@ -77,4 +81,4 @@ async def transcribe(recording: UploadFile = File(...)):
 
 
 if __name__ == "__main__":
-    uvicorn.run("__main__:APP", host="localhost", port=8000, reload=True)
+    uvicorn.run("__main__:APP", host="0.0.0.0", port=8000, reload=True)
