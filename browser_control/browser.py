14ffa9bf7d020f493793a532f299fdb5a7d05886
udithdoddapaneni <udithdoddapaneni@gmail.com>
2025-02-13 20:48:24 +0530

    fixed bugs and added try-catch where there could be exceptions

--- browser_control/browser.py
+++ browser_control/browser.py
@@ -22,16 +22,25 @@ SEARCH_URL = "https://www.bing.com/search?q="
 
 @APP.post("/browser/new_window_and_search")
 def new_window_and_search(query: SearchQuery):
-    context.new_page()
+    open_new_window()
     return search(query)
+
+@APP.post("/browser/open_new_window")
+def open_new_window():
+    try:
+        if len(context.pages) >= 5:
+            raise Exception("cannot open more windows")
+        context.new_page()
+    except Exception as e:
+        print(e)
+        return {"response": str(e)}
 
 @APP.post("/browser/search")
 def search(query: SearchQuery):
     if len(context.pages) == 0:
         return new_window_and_search()
     else:
-        query = query.query
-        context.pages[-1].goto(SEARCH_URL+query)
+        context.pages[-1].goto(SEARCH_URL+query.query)
         return {"response": f"searching for {query.query}"}
     
 @APP.post("/browser/close_current_window")
@@ -39,7 +48,6 @@ def close_current_window():
     if len(context.pages) == 0:
         return 
     context.pages[-1].close()
-    context.pages.pop()
     return {"response": "closed current window"}
 
 @APP.post("/browser/close_browser")
